3a4cf62e34536bb6f3c86dc74cb2f5ac4a8b3e0d
merodrem <j.graisse@gmail.com>
2018-08-03 23:14:19 +0200

    importing react-native-elements

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+
+import React from 'react'
+import Category from './Components/Category'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Category/>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Components/Category.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+import { Card, Button } from 'react-native-elements'
+
+class Category extends React.Component {
+    render() {
+        return (
+            <Card
+            title='HELLO WORLD'>
+            <Text style={{marginBottom: 10}}>
+            The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+            icon={{name: 'code'}}
+            backgroundColor='#03A9F4'
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='VIEW NOW' />
+            </Card>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
+
+export default Category
